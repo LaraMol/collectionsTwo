771d2592440aa1d9bb804d2c019e43c0d7d8fbd7
Lara Mol <89733492+LaraMol@users.noreply.github.com>
2022-01-17 12:07:28 +0100

    Add files via upload

--- Yahtzee.py
+++ Yahtzee.py
@@ -1,10 +1,26 @@
 import random
 
 Dobbelstenen = ['1','2','3','4','5']
-scoreboard = []
 listbfilled = [' '] * 6
 listbScores = [' '] * 6
-pogingen = 2
+
+def printResultaten(listbScores):
+    print(f"""Totaal resultaten van allen 1's: {listbScores[0]} 
+Totaal resultaten van allen 2's: {listbScores[1]} 
+Totaal resultaten van allen 3's: {listbScores[2]} 
+Totaal resultaten van allen 4's: {listbScores[3]} 
+Totaal resultaten van allen 5's: {listbScores[4]} 
+Totaal resultaten van allen 6's: {listbScores[5]} """)
+
+def printBovenste(listbfilled):
+    print (f"""---------------------------------------------
+ENEN                  |{listbfilled[0]} 
+TWEEËN                |{listbfilled[1]} 
+DRIEËN                |{listbfilled[2]} 
+VIEREN                |{listbfilled[3]} 
+VIJFEN                |{listbfilled[4]} 
+ZESSEN                |{listbfilled[5]}
+---------------------------------------------""")
 
 def upperHalfScore(Dobbelstenen:list):
     howManyDigits = []
@@ -32,36 +48,45 @@ def reroll(listRerolls,Dobbelstenen):
         Dobbelstenen[position] = roll
     return Dobbelstenen
 
-rolledDice(Dobbelstenen)
-printDice(Dobbelstenen)
-
-pogingen = 2
-while pogingen != 0:
-    rerollVraag = input(f"Wilt u rerollen? U heeft nog {pogingen} pogingen (Y/N) ")
-    if rerollVraag.lower() == "y":
-        listRerolls = []
-        while True:
-            inputReroll = int(input("Welke dobbelstenen moeten opnieuw gerolled worden? (0 is done): "))
-            if inputReroll != 0:
-                listRerolls.append(inputReroll)
-            else:
-                break
-        Dobbelstenen = reroll(listRerolls,Dobbelstenen)
-        printDice(Dobbelstenen)
-        pogingen -= 1
-    elif rerollVraag.lower() == "n":
-        break
+def loop(listbfilled):
+    if ' ' in listbfilled:
+        return True
     else:
-        print("Dat is geen optie")
+        return False
 
-listbScores = upperHalfScore(Dobbelstenen)
-print (listbScores)
 
-BovensteScore = f"""---------------------------------------------
-ENEN                  |{listbScores[0]} 
-TWEEËN                |{listbScores[1]} 
-DRIEËN                |{listbScores[2]} 
-VIEREN                |{listbScores[3]} 
-VIJFEN                |{listbScores[4]} 
-ZESSEN                |{listbScores[5]}
----------------------------------------------"""
+while loop(listbfilled):
+    rolledDice(Dobbelstenen)
+    printDice(Dobbelstenen)
+    pogingen = 2
+
+    while pogingen != 0:
+        rerollVraag = input(f"Wilt u rerollen? U heeft nog {pogingen} pogingen (Y/N) ")
+        if rerollVraag.lower() == "y":
+            listRerolls = []
+            while True:
+                inputReroll = int(input("Welke dobbelstenen moeten opnieuw gerolled worden? (0 is done): "))
+                if inputReroll != 0:
+                    listRerolls.append(inputReroll)
+                else:
+                    break
+            Dobbelstenen = reroll(listRerolls,Dobbelstenen)
+            printDice(Dobbelstenen)
+            pogingen -= 1
+        elif rerollVraag.lower() == "n":
+            break
+        else:
+            print("Dat is geen optie")
+
+    listbScores = upperHalfScore(Dobbelstenen)
+    printResultaten(listbScores)
+
+    while True:
+        invulAntwoord = int(input("Welk getal wil je invoeren?: "))
+        if listbfilled[(invulAntwoord - 1)] != ' ':
+            print('Dat is al ingevuld!')
+        else: 
+            listbfilled[(invulAntwoord - 1)] = listbScores[(invulAntwoord - 1)]
+            break
+
+    printBovenste(listbfilled)
